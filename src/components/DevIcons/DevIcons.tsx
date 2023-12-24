f881af4c844a1121c513ac07705b4038a3747a56
Mehdi S <mhdi.seddik@gmail.com>
2023-12-24 20:52:56 +0100

    wip about page

---
+++ src/components/DevIcons/DevIcons.tsx
@@ -0,0 +1,41 @@
+import type React from 'react';
+import icons from './technos.json';
+import { css, cx } from '@emotion/css';
+import Marquee from 'react-fast-marquee';
+
+export const DevIcons: React.FC = () => {
+    return (
+        <div
+            className={styles.wrapper}
+        >
+            <Marquee speed={30}>
+                {icons.map((icon, index) => (
+                    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                    <img alt="" className={cx('devicon', css`width:30px; margin-right:50px;`)} key={index} src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${icon}`} />
+                ))}
+            </Marquee>
+        </div>
+    );
+};
+
+const styles = {
+    wrapper: css`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    width: 100%;
+    overflow: hidden;
+    margin-top: 20px;
+    `,
+    deviconContainer: css`
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: row;
+    justify-content: center;
+    gap: 10px;
+    width: 100%;
+    `
+}
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -5,9 +5,10 @@ import { Navbar } from '@ui/Navbar/Navbar';
 import { Transition } from './Transition';
 import gsap from 'gsap';
 import image from '../../public/picture.png';
-import { Timeline } from '@ui/Timeline/Timeline';
 import SplitType from 'split-type';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import Lenis from '@studio-freight/lenis';
+import { DevIcons } from '@ui/DevIcons/DevIcons';
 
 export const AboutMe = () => {
   const privacyRef = useRef<HTMLDivElement>(null);
@@ -34,118 +35,160 @@ export const AboutMe = () => {
       duration: 3,
       ease: 'power4.inOut'
     });
-    // const tl = gsap.timeline();
-    // void tl.to(textRevealRef, { x: '100px', duration: 1, ease: 'power4.inOut', delay: 5 })
   }, []);
-
+  // section 2 reveal on scroll
   useEffect(() => {
-    // const smoother = new ScrollSmoother({
-    //   wrapper: '#scrollable-wrapper',
-    //   content: '#scrollable-content'
-    // });
     gsap.registerPlugin(ScrollTrigger);
-    gsap.to('#hi-im-mehdi-container', {
-      yPercent: 100,
-      ease: 'none',
+    gsap.from('#section-2-header-text .char', {
+      opacity: 0.2,
+      stagger: 3,
+      ease: 'power4.inOut',
       scrollTrigger: {
-        trigger: '#section',
-        scrub: true
+        trigger: '#section-2-header-text',
+        start: 'top 80%',
+        end: 'top 60%',
+        scrub: 1,
+        toggleActions: 'play play reverse reverse'
       }
     });
 
-    gsap.to('#job-desc-container', {
-      yPercent: 50,
-      ease: 'none',
+    gsap.from('#my-damn-sexy-pic', {
+      opacity: 0.2,
+      ease: 'power4.inOut',
       scrollTrigger: {
-        trigger: '#section',
-        scrub: true
+        trigger: '#section-2-header-text',
+        start: 'top 80%',
+        end: 'top 60%',
+        scrub: 1,
+        toggleActions: 'play play reverse reverse'
       }
     });
-  }, [])
+
+    gsap.to('#my-damn-sexy-pic', {
+      yPercent: -30,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '#section-2',
+        scrub: 1
+      }
+    });
+
+    const lenis = new Lenis();
+    lenis.on('scroll', (e: any) => {
+      console.log(e);
+    });
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+  // section 3 reveal
+
+  // useEffect(() => {
+  //   ScrollTrigger.create({
+  //     trigger: '.hold-1',
+  //     // animation: tl,
+  //     pin: true,
+  //    start: 'center center',
+  //    end: '+=1000',
+
+  //     scrub: 0, // I like the 1 sec delay, set to true for exact anime on scroll
+  //     markers: true
+  //   })
+  // }, []);
 
   return (
     <>
       <div id="scrollable-wrapper">
         <div id="scrollable-content">
+          <Navbar onNavigate={handleNavigate} />
 
-        <Navbar onNavigate={handleNavigate} />
-
-          <section id="section" className={styles.section1}>
-          <div className={styles.headerLine}>
-              <h5 className="animate-text-reveal" >{"Hi, i'm Mehdi."}</h5>
-          </div>
-            <div className={(styles.headerLine)}>
-            <h1
-              ref={textRevealRef}
+          <section id="section-1" className={styles.section1}>
+            <div className={styles.overflowHidden}>
+              <h5 className="animate-text-reveal">{"Hi, i'm Mehdi."}</h5>
+            </div>
+            <div className={styles.overflowHidden}>
+              <h1
+                ref={textRevealRef}
                 className={cx('font-bold', 'animate-text-reveal')}
               >
-              A full stack developer
-            </h1>
-          </div>
-        </section>
+                A full stack developer
+              </h1>
+            </div>
+            <DevIcons />
+          </section>
 
-        <section className={styles.section2}>
-            <h2 id="i-like-complexity" className="font-bold">
-              I like working on complex projects involving challenges and
-              innovative features
-            </h2>
+          <section id="section-2" className={cx('hold-1', styles.section2)}>
+            <div className={styles.section2HeroText}>
+              <div className={styles.overflowHidden}>
+                <h2
+                  id="section-2-header-text"
+                  className={cx('animate-text-reveal', 'font-bold')}
+                >
+                  Code passionate
+                </h2>
+              </div>
+              <div className={styles.overflowHidden}>
+
+                <h2
+                  id="section-2-header-text"
+                  className={cx('animate-text-reveal', 'font-bold')}
+                >
+                  Eager learner
+                </h2>
+              </div>
+              <div className={styles.overflowHidden}>
+                <h2
+                  id="section-2-header-text"
+                  className={cx('animate-text-reveal', 'font-bold')}
+                >
+                  Hard worker
+                </h2>
+              </div>
+            </div>
+            <img
+              id="my-damn-sexy-pic"
+              src={image}
+              alt=""
+              className={styles.image}
+            />
+          </section>
+          <section id="section-3" className={styles.section3}>
+            <div className={styles.techHeader}>
+              <img alt="" className={cx(css`width:500px;`)} src={'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'} />
+              <div className={styles.techWrapper}>
+
+                <h2 className="font-bold">
+                  {"I'm in love with"} <br /> {'React'}
+                </h2>
+              </div>
+            </div>
+          </section>
+          <section>
+            But i can also code with...
+          </section>
+
+          {/* <section className={styles.section3}>
             <div
               className={css`
-            display: flex;
-            flex-direction: row;
-            gap: 50px;
-            padding: 50px 0px 0px 0px;
-            `}
+                display: flex;
+                flex-direction: row;
+                gap: 50px;
+                padding: 50px 0px 0px 0px;
+              `}
             >
-              <img src={image} alt="" className={styles.image} />
               <div>
                 <Timeline />
               </div>
             </div>
-          </section>
+          </section> */}
 
-          <section className={styles.section2}>
-            <h2 id="i-like-complexity" className="font-bold">
-            I like working on complex projects involving challenges and
-            innovative features
-          </h2>
-          <div
-            className={css`
-            display: flex;
-            flex-direction: row;
-            gap: 50px;
-            padding: 50px 0px 0px 0px;
-            `}
-            >
-            <img src={image} alt="" className={styles.image} />
-            <div>
-              <Timeline />
-            </div>
-          </div>
-        </section>
-
-        <section className={styles.section2}>
-          <h2 className="font-bold">
-            I use the latest technologies to build awesome products
-          </h2>
-          <img
-            alt=""
-            width="50px"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-            />
-
-          <img
-            alt=""
-            width="50px"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-            />
-        </section>
-
-      </div>
-      <Transition
-        title="Mehdi Seddik"
-        privacyRef={privacyRef}
-        titleRef={titleRef}
+        </div>
+        <Transition
+          title="Mehdi Seddik"
+          privacyRef={privacyRef}
+          titleRef={titleRef}
         />
       </div>
     </>
@@ -153,8 +196,26 @@ export const AboutMe = () => {
 };
 
 const styles = {
-  headerLine: css`
+  techWrapper: css`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: start;
+    gap: 20px;
+  `,
+  techHeader: css`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  `,
+  section2HeroText: css`
+    display: flex;
+    flex-direction: column;
+  `,
+  overflowHidden: css`
     overflow: hidden;
+    padding:5px;
   `,
   section1: css`
     height: 100vh;
@@ -165,18 +226,29 @@ const styles = {
     align-items: center;
   `,
   section2: css`
+    width: 100%;
+    height: 100vh;
+    padding: 0px 100px 0px 80px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 50px;
+  `,
+  section3: css`
     height: 100vh;
     width: 100vw;
     padding: 0px 80px 0px 80px;
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
   `,
   heroContent: css`
     display: flex;
     flex-direction: column;
   `,
   image: css`
-    width: 350px;
-    margin: 50 auto;
+    width: 400px;
   `
 };
