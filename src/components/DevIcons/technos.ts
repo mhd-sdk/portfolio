0ade3a3d76851dbee6af6542ebb9511f97b7b39b
Mehdi S <mhdi.seddik@gmail.com>
2023-12-26 21:28:42 +0100

    wip tech list

--- src/components/DevIcons/technos.ts
+++ src/components/DevIcons/technos.ts
@@ -1,70 +1,147 @@
 export const technos = [
   {
+    name: 'JavaScript',
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
-    type: 'language'
+    type: 'language',
   },
   {
+    name: 'TypeScript',
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
-    type: 'language'
+    type: 'language',
   },
   {
+    name: 'React',
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
-    type: 'librarie'
+    type: 'library',
   },
   {
+    name: 'Go',
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg',
-    type: 'language'
+    type: 'language',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg',
-    type: 'language'
+    type: 'language',
+    name: 'Bash',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain.svg',
-    type: 'devops'
+    type: 'devops',
+    name: 'Ubuntu',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg',
-    type: 'devops'
+    type: 'devops',
+    name: 'Linux',
   },
   {
     url: 'https://gofiber.io/assets/images/logo.svg',
-    type: 'framework'
+    type: 'framework',
+    name: 'Fiber',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
-    type: 'librarie'
+    type: 'library',
+    name: 'Bootstrap',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
-    type: 'devops'
+    type: 'devops',
+    name: 'Git',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
-    type: 'devops'
+    type: 'devops',
+    name: 'Github',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/symfony/symfony-original.svg',
-    type: 'framework'
+    type: 'framework',
+    name: 'Symfony',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg',
-    type: 'language'
+    type: 'language',
+    name: 'PHP',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
-    type: 'database'
+    type: 'database',
+    name: 'MySQL',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
-    type: 'database'
+    type: 'database',
+    name: 'MongoDB',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
-    type: 'database'
+    type: 'database',
+    name: 'PostgreSQL',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg',
-    type: 'language'
-  }
+    type: 'language',
+    name: 'GraphQL',
+  },
+  {
+    name: 'Python',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+    type: 'language',
+  },
+  {
+    name: 'Docker',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+    type: 'devops',
+  },
+  {
+    name: 'Storybook',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/storybook/storybook-original.svg',
+    type: 'library',
+  },
+  {
+    name: 'Jest',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
+    type: 'library',
+  },
+  {
+    name: 'Figma',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg',
+    type: 'library',
+  },
+  {
+    name: 'Postman',
+    url: 'https://www.svgrepo.com/show/354202/postman-icon.svg',
+    type: 'library',
+  },
+  {
+    name: 'VSCode',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg',
+    type: 'library',
+  },
+  {
+    name: 'Apache',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apache/apache-original.svg',
+    type: 'devops',
+  },
+  {
+    name: 'Nginx',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg',
+    type: 'devops',
+  },
+  {
+    name: 'NodeJS',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+    type: 'language',
+  },
+  {
+    name: 'Express',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
+    type: 'framework',
+  },
+  {
+    name: 'MaterialUI',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg',
+    type: 'library',
+  },
 ]
--- src/components/techSearch/TechSearch.tsx
+++ src/components/techSearch/TechSearch.tsx
@@ -3,51 +3,48 @@ import { technos } from '@ui/DevIcons/technos'
 import Atropos from 'atropos/react'
 import 'atropos/css'
 import { useState } from 'react'
+import { darkTheme, lightTheme } from '../../dim-theme'
+import { useTheme } from '../../hooks/useTheme'
+import gsap from 'gsap'
 export const TechSearch = (): JSX.Element => {
-  const [selectedFilters, setSelectedFilters] = useState<string[]>([])
-  const handleChangeFilters = (filter: string) => {
-    if (selectedFilters.includes(filter)) {
-      setSelectedFilters(selectedFilters.filter((f) => f !== filter))
-    }
-    setSelectedFilters([...selectedFilters, filter])
-  }
+  const [selectedType, setSelectedType] = useState<'language' | 'library' | 'framework' | 'devops' | 'databaset'>('language')
+  const filteredTechnos = technos.filter(techno => techno.type === selectedType)
+  const { theme } = useTheme();
   return (
     <div className={styles.wrapper}>
       <div className={styles.filterWrapper}>
         <div role="tablist" className={cx('tabs tabs-boxed', styles.filter)}>
           <button
-            onClick={() => handleChangeFilters('language')}
+            onClick={() => setSelectedType('language')}
             role="tab"
-            className="tab btn-active"
+            className={cx("tab", selectedType === "language" && "btn-active")}
           >
             Langages
           </button>
-          <button role="tab" className="tab">
+          <button className={cx("tab", selectedType === "library" && "btn-active")} onClick={() => setSelectedType('library')}>
             Libraries
           </button>
-          <button role="tab" className="tab">
+          <button role="tab" className={cx("tab", selectedType === "framework" && "btn-active")} onClick={() => setSelectedType('framework')}>
             Frameworks
           </button>
-          <button role="tab" className="tab">
+          <button role="tab" className={cx("tab", selectedType === "devops" && "btn-active")} onClick={() => setSelectedType('devops')}>
             Devops
-          </button>
-        </div>
-        <div role="tablist" className={cx('tabs tabs-boxed', styles.filter)}>
-          <button role="tab" className="tab btn-active">
-            Frontend
-          </button>
-          <button role="tab" className="tab">
-            Backend
           </button>
         </div>
       </div>
       <div className={styles.techList} id="tech-filtered-list">
-        {technos.map((icon, index) => (
+        {filteredTechnos.map((icon, index) => (
           <Atropos
             key={index}
             className={styles.atroposCard}
             shadow={false}
             highlight={false}
+            onMouseEnter={() => {
+              gsap.to(`#icon-${icon.name}`, { opacity: 1, duration: 0.2, ease: "power1.inOut" })
+            }}
+            onMouseLeave={() => {
+              gsap.to(`#icon-${icon.name}`, { opacity: 0, duration: 0.2, ease: "power1.inOut" })
+            }}
           >
             <div
               className={css`
@@ -67,6 +64,9 @@ export const TechSearch = (): JSX.Element => {
                 key={index}
                 src={icon.url}
               />
+              <p id={`icon-${icon.name}`} data-atropos-offset={7} className={styles.techName(theme)}>
+                {icon.name}
+              </p>
             </div>
           </Atropos>
         ))}
@@ -76,15 +76,25 @@ export const TechSearch = (): JSX.Element => {
 }
 
 const styles = {
+  techName: (theme: string) => css`
+    position:absolute;
+    background:${theme === "dim" ? darkTheme['base-100'] : lightTheme['base-200']}; 
+    bottom: 0px;
+    border-radius: 5px;
+    padding: 0px 2px;
+    opacity: 0;
+  `,
   techList: css`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
+    gap: 20px;
   `,
   atroposCard: css`
-    width: 100px;
-    height: 100px;
+    width: 90px;
+    height: 80px;
     background-color: none;
+    
   `,
   filterWrapper: css`
     bottom: 0px;
